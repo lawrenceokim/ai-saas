70dee356a847914d0b4f3d40f7ba3c34404a431f
lawrenceokim <lawrence4okim@gmail.com>
2024-07-28 14:26:18 +0100

    mobile sidebar done + functionality

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"; //sheet was installed from shadcn
+import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
+
+function MobileSidebar() {
+  //preventing hydration error: it's due to client looking different from what was sent to the server
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default MobileSidebar;
